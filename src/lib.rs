27f7a042fa2c700f83f3903beaab63c421a4153c
Adelbert Chang <achang@octoml.ai>
2020-10-16 13:37:05 -0700

    initial code from opentelemetry-rust at 0ea5ed697455659087f7f321c64de25e3b479db9

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # OpenTelemetry Contrib
+//!
+//! This is a library for extensions that are not part of the core API, but still may be useful for
+//! some users.
+//!
+//! Typically, those include vendor specific propagators.
+#[cfg(feature = "datadog")]
+pub mod datadog;
